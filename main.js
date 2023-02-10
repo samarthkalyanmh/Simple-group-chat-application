56ea9173b80f38ebb1d5444a03c7fff8e69ed6cb
samarthkalyanmh <samarthkalyanmh777@gmail.com>
2023-02-10 15:13:11 +0530

    final simple group chat app

--- main.js
+++ main.js
@@ -6,19 +6,13 @@ const app = express()
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-
+//removed savedetails form action="/savedetails"
 app.get('/login', (req,res,next)=>{
-    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/savedetails" method="POST"><input id="username" type="text" placeholder="username" name="username"><button type="submit">Login</button></form>')
+    res.send('<form onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" action="/" ><input id="username" type="text" placeholder="username" name="username"><button type="submit">Login</button></form>')
 })
 
 //save username in local storage and redirect to chat window
-app.post('/savedetails', (req,res,next)=>{
-
-    res.redirect('/')
-    console.log(req.body)
-})
-
-app.post('/writechat', (req,res,next)=>{
+app.post('/', (req,res,next)=>{
    
     console.log(req.body)
 
@@ -28,21 +22,21 @@ app.post('/writechat', (req,res,next)=>{
     
 })
 
-app.use('/',(req,res,next)=>{
+app.get('/',(req,res,next)=>{
 
     fs.readFile('chats.txt', (err, data) => {
         data = data.toString()
         if(data === ''){
-            res.send('<p>No chats exist</p><form onsubmit="document.getElementById(`hidden`).value = localStorage.getItem(`username`)" action="/writechat" method="POST"><input type="text" placeholder="enter message" name="message"><input id="hidden" type="hidden" name="hidden"><button type="submit">Send</button></form>')
+            res.send('<p>No chats exist</p><form onsubmit="document.getElementById(`hidden`).value = localStorage.getItem(`username`)" action="/" method="POST"><input type="text" placeholder="enter message" name="message"><input id="hidden" type="hidden" name="hidden"><button type="submit">Send</button></form>')
         }
         else{
-            res.send(`<p>${data}</p><form onsubmit="document.getElementById('hidden').value  = localStorage.getItem('username')" action="/writechat" method="POST"><input type="text" placeholder="enter message" name="message"><input id="hidden" type="hidden" name="hidden"><button type="submit">Send</button></form>`)
+            res.send(`<p>${data}</p><form onsubmit="document.getElementById('hidden').value  = localStorage.getItem('username')" action="/" method="POST"><input type="text" placeholder="enter message" name="message"><input id="hidden" type="hidden" name="hidden"><button type="submit">Send</button></form>`)
         }
     })   
 })
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1><a href="/login"><button>Login Page</button></a>')
+    // res.redirect('/')
 })
 
 app.listen(3000)
